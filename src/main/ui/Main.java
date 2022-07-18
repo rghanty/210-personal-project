b59d9120fda1f10a2c70147feaa968e89d026500
Rishabh Ghanty <rishabhg260203@gmail.com>
2022-07-17 17:50:12 -0700

    Changed data structure of prescription to maintain order.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -10,17 +10,6 @@ public class Main {
     public static void main(String[] args) {
         //new PrescriptifyApp();
 
-        Prescription p;
-        Medicine m1 = new Medicine("Paracetamol",1);
-        Medicine m2 = new Medicine("PAN-D", 1);
-        Medicine m3 = new Medicine("ABC", 1);
-
-        p = new Prescription();
-        p.addMedTime(m1,20);
-        p.addMedTime(m2,10);
-        p.addMedTime(m3, 15);
-
-        System.out.println(p.viewMeds());
 
 
        //Date date = new Date();
